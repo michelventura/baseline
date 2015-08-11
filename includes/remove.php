83671f6e6758340f64c0de9848b228725e6d77a6
Mike Hemberger <jivetheuniversal@gmail.com>
2015-08-11 16:44:26 -0400

    Update remove.php

--- includes/remove.php
+++ includes/remove.php
@@ -1,12 +1,20 @@
 <?php
 
-//* Unregister layout settings
+// Unregister layout settings
 genesis_unregister_layout( 'content-sidebar-sidebar' );
 genesis_unregister_layout( 'sidebar-content-sidebar' );
 genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-//* Unregister secondary sidebar
+// Unregister secondary sidebar
 unregister_sidebar( 'sidebar-alt' );
 
-//* Remove the edit link
+// Remove the edit link
 add_filter ( 'genesis_edit_post_link' , '__return_false' );
+
+// Remove Blog & Archive Template From Genesis
+add_filter( 'theme_page_templates', 'jivedig_remove_page_templates' );
+function jivedig_remove_page_templates( $templates ) {
+	unset( $templates['page_blog.php'] );
+	unset( $templates['page_archive.php'] );
+	return $templates;
+}
