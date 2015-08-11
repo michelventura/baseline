8a7765453b33d90526ce05d575c281472ed1f65a
Mike Hemberger <jivetheuniversal@gmail.com>
2015-08-11 16:48:08 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,22 @@
 <?php
-//* Start the engine
+// Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
-//* Child theme (do not remove)
+// Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Baseline' );
 define( 'CHILD_THEME_URL', 'http://thestizmedia.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.7' );
 
-//* Add HTML5 markup structure
+// Add HTML5 markup structure
 add_theme_support( 'html5' );
 
-//* Add viewport meta tag for mobile browsers
+// Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
-//* Add support for 3-column footer widgets
+// Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 2 );
 
-//* Enqueue Javascript files
+// Enqueue Javascript files
 add_action( 'wp_enqueue_scripts', 'baseline_enqueue_scripts' );
 function baseline_enqueue_scripts() {
 	// Sidr slide out menu
@@ -24,11 +24,11 @@ function baseline_enqueue_scripts() {
 	wp_enqueue_script( 'baseline-global', get_stylesheet_directory_uri() . '/assets/js/global.js', array( 'sidr' ), '1.0.0', true );
 }
 
-//* Enqueue CSS files
+// Enqueue CSS files
 add_action( 'wp_enqueue_scripts', 'baseline_enqueue_styles' );
 function baseline_enqueue_styles() {
 	wp_enqueue_style( 'baseline-google-fonts', '//fonts.googleapis.com/css?family=Roboto:300,300italic,400,400italic,700,700italic|Roboto+Condensed:400,700', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), '4.3.0' );
+	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', array(), '4.4.0' );
 }
 
 /**
@@ -45,23 +45,23 @@ include_once('includes/images.php');
 include_once('includes/navigation.php');
 include_once('includes/remove.php');
 
-//* Activate After Entry widget area and display it on single posts
+// Activate After Entry widget area and display it on single posts
 add_theme_support( 'genesis-after-entry-widget-area' );
 
-//* Customize the entry meta in the entry header
+// Customize the entry meta in the entry header
 add_filter( 'genesis_post_info', 'baseline_post_info_filter' );
 function baseline_post_info_filter($post_info) {
 	$post_info = '[post_date] [post_author_posts_link] [post_comments] [post_edit]';
 	return $post_info;
 }
 
-//* Modify the size of the Gravatar in the author box
+// Modify the size of the Gravatar in the author box
 add_filter( 'genesis_author_box_gravatar_size', 'author_box_gravatar_size' );
 function author_box_gravatar_size( $size ) {
 	return '200';
 }
 
-//* Modify the size of the Gravatar in comments
+// Modify the size of the Gravatar in comments
 add_filter( 'genesis_comment_list_args', 'sp_comments_gravatar' );
 function sp_comments_gravatar( $args ) {
 	$args['avatar_size'] = 160;
