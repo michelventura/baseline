7ef0099f47c0696205052f9a18e5614d042ab7f4
Mike Hemberger <mike@thestizmedia.com>
2016-10-24 14:48:07 -0400

    flexington vbump to 2.3.1

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( get_template_directory() . '/lib/init.php' );
 // Child theme (do not remove)
 define( 'CHILD_THEME_NAME', 'Baseline' );
 define( 'CHILD_THEME_URL', 'https://bizbudding.com/' );
-define( 'CHILD_THEME_VERSION', '2.5.6' );
+define( 'CHILD_THEME_VERSION', '2.5.7' );
 
 // Add HTML5 markup structure
 add_theme_support( 'html5' );
@@ -47,7 +47,7 @@ add_action( 'wp_enqueue_scripts', 'prefix_enqueue_styles' );
 function prefix_enqueue_styles() {
 	wp_enqueue_style( 'prefix-google-fonts', '//fonts.googleapis.com/css?family=Lato:400,400i,700|Source+Sans+Pro:400,400i,700', array(), CHILD_THEME_VERSION );
 	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', array(), '4.6.3' );
-	wp_enqueue_style( 'flexington', get_stylesheet_directory_uri() . '/assets/css/flexington.min.css', array(), '2.3.0' );
+	wp_enqueue_style( 'flexington', get_stylesheet_directory_uri() . '/assets/css/flexington.min.css', array(), '2.3.1' );
 }
 
 /**
